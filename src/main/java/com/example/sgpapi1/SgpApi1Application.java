6bd65c6399020c88174b590a560e4e6b604102f1
Valentin Bourdaud <bourdaudv@gmail.com>
2018-06-01 09:40:04 +0200

    Creation projet

---
+++ src/main/java/com/example/sgpapi1/SgpApi1Application.java
@@ -0,0 +1,12 @@
+package com.example.sgpapi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpApi1Application.class, args);
+	}
+}
